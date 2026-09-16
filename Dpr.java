c4c8d3dabccf14c628d7b5364fe8bcf8ac78ef3e
Erik-Marques1 <erik.marques@sou.unifeob.edu.br>
2026-09-16 16:13:10 -0300

    Update and rename dpr.java to Dpr.java

--- Dpr.java
+++ Dpr.java
@@ -1,4 +1,4 @@
-public class dpr {
+public class Dpr {
 
     // ATRIBUTOS
     private String lote;
@@ -17,7 +17,7 @@ public class dpr {
 
 
     // CONSTRUTOR
-    public dpr(
+    public Dpr(
             String lote,
             String materiaPrima,
             String produto,
